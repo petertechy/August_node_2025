ac22512d2a7c2fd15a3d976d1ad8ca97e0385bca
Petertechy <petertechy01@gmail.com>
2025-09-02 15:55:05 +0100

    Add models

---
+++ models/userModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose")
+
+
+//User Schema
+const userSchema = mongoose.Schema({
+    firstname: {type: String, required: true},
+    lastname: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    register_date: {type: Date, default: Date.now()}
+})
+
+//User Model
+
+const userModel = mongoose.model("User_Data", userSchema, "User_Data")
+
+module.exports = userModel
