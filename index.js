66cbf5dcd190762a3152055b8013598e3007c71a
Petertechy <petertechy01@gmail.com>
2025-09-02 15:55:04 +0100

    Setup Express entry point

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+const ejs = require("ejs")
+const mongoose = require("mongoose")
+const userRoute = require("./routes/user.route")
+app.set("view engine", "ejs")
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use("/",userRoute)
+
+const URI = "mongodb+srv://petertechy01:Olanrewaju@my-project.w3cyijl.mongodb.net/school_portal?retryWrites=true&w=majority&appName=my-project"
+
+mongoose.connect(URI)
+.then(()=>{
+    console.log("Mongodb iyaf connected")
+})
+.catch((err)=>{
+    console.log("Unable to connect to mongodb")
+    console.log(err)
+})
+
+app.listen(PORT, ()=>{
+    console.log("Server is Running on port " + PORT)
+})
