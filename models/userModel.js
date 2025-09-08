5237160edf3a3300b298b9b9c27e81d63d09e704
Petertechy <petertechy01@gmail.com>
2025-09-08 16:44:23 +0100

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcryptjs")
 
 //User Schema
 const userSchema = mongoose.Schema({
@@ -10,8 +10,33 @@ const userSchema = mongoose.Schema({
     register_date: {type: Date, default: Date.now()}
 })
 
-//User Model
 
+let saltRound = 10
+userSchema.pre("save", function(next){
+    bcrypt.hash(this.password,saltRound, (err,hashedPassword)=>{
+        if(err){
+            console.log(err, "Password could not be hashed")
+        }else{
+            this.password = hashedPassword
+            next()
+        }
+    })
+})
+
+userSchema.methods.validatePassword = function(password,callback){
+    console.log(password, this.password)
+    bcrypt.compare(password,this.password, (err,same)=>{
+        if(!err){
+            console.log(same)
+            callback(err,same)
+        }
+        else{
+            next()
+        }
+    })
+}
+
+//User Model
 const userModel = mongoose.model("User_Data", userSchema, "User_Data")
 
 module.exports = userModel
